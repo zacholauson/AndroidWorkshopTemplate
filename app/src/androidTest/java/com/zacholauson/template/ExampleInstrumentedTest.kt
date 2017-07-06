45344c8bf52ad549443da963de57b2968ebb60cb
Zach Olauson <zmolauson@gmail.com>
2017-07-05 21:31:12 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/zacholauson/template/ExampleInstrumentedTest.kt
@@ -0,0 +1,18 @@
+package com.zacholauson.template
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.zacholauson.template", appContext.packageName)
+    }
+}
